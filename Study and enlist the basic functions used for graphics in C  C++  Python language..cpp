e908ca63692a561f350e574c69d9652a000f05a8
Dullboyzz <149328706+Dullboyzz@users.noreply.github.com>
2024-03-25 14:28:59 +0530

    Add files via upload

--- Study and enlist the basic functions used for graphics in C  C++  Python language..cpp
+++ Study and enlist the basic functions used for graphics in C  C++  Python language..cpp
@@ -1,85 +1,21 @@
 /*
 Aim: Study and enlist the basic functions used for graphics in C language
-
-//Arc Function in C:
-
-#include<graphics.h>
-#include<conio.h>
-int main(){
-int gd=DETECT,gm;
-initgraph(&gd,&gm,"C:\\TURBOC\\BGI");
-arc(100,100,0,135,50);
-getch();
-closegraph();
-}
-
-//Drawpoly Function in C:
-
-#include<graphics.h>
-#include<conio.h>
-int main(){
-int gd=DETECT,gm,points[]={320,150,420,300,250,300,320,150};
-initgraph(&gd,&gm,"C:\\TURBOC\\BGI");
-drawpoly(4,points);
-getch();
-closegraph();
-}
-
-//Circle Function in C:
-
-#include<graphics.h>
-#include<conio.h>
-int main(){
-int gd=DETECT,gm;
-initgraph(&gd,&gm,"C:\\TURBOC\\BGI");
-circle(100,100,50);
-getch();
-closegraph();
-}
-
-//Cleardevice Function in C:
-
-#include<graphics.h>
-#include<conio.h>
-int main(){
-int gd=DETECT,gm;
-initgraph(&gd,&gm,"C:\\TURBOC\\BGI");
-outtext("press any key to clear screen");
-getch();
-cleardevice();
-outtext("press any key to exit");
-getch();
-closegraph();
-}
-
-//Floodfill Function in C:
-
-#include<graphics.h>
-#include<conio.h>
-int main()
-{
-int gd=DETECT,gm;
-initgraph(&gd,&gm,"C:\\TURBOC\\BGI");
-setcolor(RED);
-circle(100,100,50);
-floodfill(100,100,RED);
-getch();
-closegraph();
-}
-
-//Setcolor Function in C:
-
-#include<graphics.h>
-#include<conio.h>
-int main()
-{
-int gd=DETECT,gm;
-initgraph(&gd,&gm,"C:\\TURBOC\\BGI");
-circle(100,100,50);
-setcolor(RED);
-circle(200,200,50);
-getch();
-closegraph();
-}
-
 */
+
+#include <graphics.h>
+#include<conio.h>
+main(){
+
+int
+gd=DETECT,gm,left=100,top=100,right=200,bottom=200,x=300,y=150,radius=50;
+initgraph(&gd,& gm,"C:\\TURBOC3\\BGI");
+rectangle(left,top,right,bottom);
+circle(x,y,radius);
+bar(left+300,top,right+300,bottom);
+line(left-10,top+150,left+410,top+150);
+ellipse(x,y+200,0,360,100,50);
+outtextxy(left+100,top+325, "My First C Graphics program");
+getch();
+closegraph();
+return 0;
+}
